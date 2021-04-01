a9c3e7dfbf166935f61850dd4f445080d8bf912e
munir <mnrmr9@gmailcom>
2021-04-01 00:47:19 -0600

    this is created by munir

--- Demo.py
+++ Demo.py
@@ -2,7 +2,7 @@ print("Hello World")
 
 first_name = "karim"
 last_name = "James"
-name = first_name, last_name
+name = first_name + last_name
 print(name)
 goals = "I want to learn coding to work in penetration testing"
 print(goals)
